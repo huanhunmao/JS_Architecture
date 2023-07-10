3a329a4cc49bf815757d3f26f68c9cb6762a05e3
“fuhaojun” <fuhaojun@52tt.com>
2023-07-10 10:00:27 +0800

    feat: add demo

---
+++ js_demo/js_chekout /console.js
@@ -0,0 +1,2 @@
+// 1
+console.log(null || 'string')  // string
---
+++ js_demo/js_chekout /if.js
@@ -0,0 +1,10 @@
+// 2 if  BA -> 保安
+let income = 5500  
+
+if(income > 3000){
+    var tax = 500
+}else{
+    var tax = 200 
+}
+
+console.log('tax', tax) // tax 500
